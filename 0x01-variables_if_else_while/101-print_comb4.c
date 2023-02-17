3dfbdc1625d8352f859c5454c7c0a6250a81ecb2
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-17 13:04:50 +0100

    Correction

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,18 +11,18 @@
 int main(void)
 {
 int i, j, k;
-for(i = 48; i < 58; i++)
+for (i = 48; i < 58; i++)
 {
-for(j = i; j < 58; j++)
+for (j = i; j < 58; j++)
 {
-for(k = j; k < 58; k++)
+for (k = j; k < 58; k++)
 {
-if(i != j && i != k && j != k)
+if (i != j && i != k && j != k)
 {
 putchar(i);
 putchar(j);
 putchar(k);
-if(i == 55 && j == 56 && k == 57)
+if (i == 55 && j == 56 && k == 57)
 {
 }
 else{
