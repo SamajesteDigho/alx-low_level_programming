5be351bc7e5149a7dc8a992418fbf27b2b3222a6
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-17 12:46:51 +0100

    Correction

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,16 +4,16 @@
 /**
  * main - Entry point
  *
- * Description: 'Prints all single digit numbers of base 10 starting from 0, followed by a new line'
+ * Description: 'Prints all single digit numbers of base 10 starting from 0'
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 int i;
-for(i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
-printf("%d",i);
+printf("%d", i);
 }
 putchar('\n');
 return (0);
