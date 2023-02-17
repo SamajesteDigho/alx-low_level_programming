f8f3e9e7beec8e02ca6097da02310860741db465
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-17 12:49:39 +0100

    Correction

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,16 +4,16 @@
 /**
  * main - Entry point
  *
- * Description: 'Prints all the numbers of base 16 in lowercase, followed by a new line'
+ * Description: 'Prints all the numbers of base 16 in lowercase.'
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 int i;
-for(i = 48; i < 64; i++)
+for (i = 48; i < 64; i++)
 {
-if(i <= 57){
+if (i <= 57){
 putchar(i);
 }
 else
