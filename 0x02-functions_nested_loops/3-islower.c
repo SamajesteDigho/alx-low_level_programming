76b9b1d4b411ec051c97a918413b63718d9c6972
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-21 22:33:42 +0100

    Commiting

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - Lower case
+ *
+ * Description: A program that prints the alphabet
+ */
+int _islower(int c)
+{
+
+if (c >= 97 && i <= 122)
+{
+return (1);
+}
+return (0);
+}
