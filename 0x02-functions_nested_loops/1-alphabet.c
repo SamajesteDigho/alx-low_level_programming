21c403fb70eaa2b0c000b9ff0ac86d715c2bc9a1
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-21 22:07:43 +0100

    Commiting

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: 'A program tat prints _putchar'
+ * Description: 'A program that prints the alphabet'
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 int i;
-for(i = 97; i < 123; i++)
+for (i = 97; i < 123; i++)
 {
 _putchar(i);
 }
