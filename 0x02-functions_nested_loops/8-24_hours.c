162666ffaf1d43035ba7937885774481408c96cb
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-21 23:24:06 +0100

    Commiting

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * jack_bauer - print last digit
  *
- * @n: n is the parameter
- *
  * Description: A program that prints the alphabet
  */
-int jack_bauer(int n)
+void jack_bauer()
 {
-
+int i, j, k, l;
+for (i = 0; i < 3 ; i++)
+{
+for (j = 0; j < 4 ; j++)
+{
+for (k = 0; k < 6 ; k++)
+{
+for (l = 0; l < 10 ; l++)
+{
+_putchar(48 + i);
+_putchar(48 + j);
+_putchar(':');
+_putchar(48 + k);
+_putchar(48 + l);
+_putchar('\n');
+}
+}
+}
+}
 }
