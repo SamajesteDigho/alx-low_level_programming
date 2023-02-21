00e29f89f885f6cebc7eed54164ab14ea6b632da
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-21 22:54:28 +0100

    Commiting

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - Sign of number
+ *
+ * @c: c is the parameter
+ *
+ * Description: A program that prints the alphabet
+ *
+ * Return: Either -1 or 0 or 1
+ */
+int print_sign(int n)
+{
+if (c > 0)
+{
+return (1);
+}
+if (c == 0)
+{
+return (1);
+}
+return (-1);
+}
