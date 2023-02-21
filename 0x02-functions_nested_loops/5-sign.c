38c2e1ccc9d0369d019574fe0abd8e5982d81cd3
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-21 22:58:05 +0100

    Commiting

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,11 +10,11 @@
  */
 int print_sign(int n)
 {
-if (c > 0)
+if (n > 0)
 {
 return (1);
 }
-if (c == 0)
+if (n == 0)
 {
 return (1);
 }
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Absolute number o integer
+ *
+ * @c: c is the parameter
+ *
+ * Description: A program that prints the alphabet
+ *
+ * Return: Positive number
+ */
+int _abs(int n)
+{
+if (c >= 0)
+{
+return (n);
+}
+return (n * -1);
+}
