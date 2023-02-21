32460c9284bf75c8faef2c7137d05fe30c444b01
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-21 23:07:41 +0100

    Commiting

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ *
+ * @n: n is the parameter
+ *
+ * Description: A program that prints the alphabet
+ *
+ * Return: The ast digit of a number
+ */
+int print_last_digit(int n)
+{
+return (_abs(n) % 10);
+}
