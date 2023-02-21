75243d42ff105fdc19d79edf59b1cfb1f2f500f6
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-21 23:40:43 +0100

    Commiting

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,12 +12,12 @@ int print_last_digit(int n)
 {
 int i;
 if(n >= 0){
-i = 48 + (n % 10);
-_putchar(i);
+i = (n % 10);
+_putchar(48 + i);
 }
 else{
-i = 48 + ((n * -1) % 10);
-_putchar(i);
+i = ((n * -1) % 10);
+_putchar(48 + i);
 }
 return (i);
 }
