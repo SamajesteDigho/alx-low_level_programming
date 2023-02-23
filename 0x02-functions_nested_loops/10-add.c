b04036a11ac5f7acbd0f2e8bb340e3cbfe4146fa
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-23 03:47:52 +0100

    Commiting

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - print last digit
+ * @a : parameter
+ * @b : parameter
+ * Description: A program that prints the alphabet
+ * Return: Returns the sum of a and b
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - print last digit
+ *
+ * Description: A program that prints the alphabet
+ */
+void _putchar(int c)
+{
+putchar(c);
+}
+void times_table(void)
+{
+int i, j;
+for (i = 0; i < 10 ; i++)
+{
+for (j = 0; j < 10 ; j++)
+{
+_putchar(48 + (i * j));
+if(j != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
+
+int main(void)
+{
+    times_table();
+    return (0);
+}
