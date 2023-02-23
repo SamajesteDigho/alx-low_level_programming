73a613542c3705143e5355a22d28d960839633e6
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-23 05:42:11 +0100

    Commiting

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,12 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_times_table - print last digit
- * @n : parameter
+ * main - print last digit
  * Description: A program that prints the alphabet
+ * Return: Always return 0.
  */
-
-int main()
+int main(void)
 {
 int i, res;
 for (i = 0; i <= 1024; i++)
@@ -20,11 +19,17 @@ printf("%d\n", res);
 return (0);
 }
 
+/**
+ * is_multiple_of_3_or_5 - print last digit
+ * @n : parameter
+ * Description: A program that prints the alphabet
+ * Return: Return a value.
+ */
 int is_multiple_of_3_or_5(int n)
 {
 if (n % 3 == 0 || n % 5 == 0)
 {
-return 1;
+return (1);
 }
-return 0;
+return (0);
 }
