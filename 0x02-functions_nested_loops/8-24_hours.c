878be9cc815fc2bc58a3b56b0d6f63d4535ff864
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-23 03:34:53 +0100

    Commiting

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * jack_bauer - print last digit
  *
@@ -10,11 +9,13 @@ void jack_bauer(void)
 int i, j, k, l;
 for (i = 0; i < 3 ; i++)
 {
-for (j = 0; j < 4 ; j++)
+for (j = 0; j < 10 ; j++)
 {
 for (k = 0; k < 6 ; k++)
 {
 for (l = 0; l < 10 ; l++)
+{
+if(i*10 + j < 24)
 {
 _putchar(48 + i);
 _putchar(48 + j);
@@ -22,6 +23,7 @@ _putchar(':');
 _putchar(48 + k);
 _putchar(48 + l);
 _putchar('\n');
+}
 }
 }
 }
