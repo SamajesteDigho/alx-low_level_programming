1630bcfa9ccbd5d55121c0a02f115f579366a84c
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-23 04:12:43 +0100

    Commiting

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,8 +16,18 @@ for (i = 0; i < 10 ; i++)
 {
 for (j = 0; j < 10 ; j++)
 {
-_putchar(48 + (i * j));
-if(j != 9)
+if(i * j >= 10)
+{
+_putchar(48 + ((i * j) / 10));
+_putchar(48 + ((i * j) % 10));
+
+}
+else
+{
+_putchar(' ');
+_putchar(48 + ((i * j) % 10));
+}
+if (j != 9)
 {
 _putchar(',');
 _putchar(' ');
