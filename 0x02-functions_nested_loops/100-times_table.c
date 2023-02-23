718f9abcff6bbf6528875583ecbc265a01d099b1
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-23 05:28:48 +0100

    Commiting

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,10 +7,9 @@
 void print_times_table(int n)
 {
 int i, j;
-
 if (n < 0 || n > 15)
 {
-return ;
+return;
 }
 for (i = 0; i < n + 1 ; i++)
 {
@@ -18,10 +17,8 @@ for (j = 0; j < n + 1 ; j++)
 {
 if (i * j >= 100)
 {
-int f = (i * j) / 100;
-int s = ((i * j) - (f * 100)) / 10;
-_putchar(48 + f);
-_putchar(48 + s);
+_putchar(48 + (i * j) / 100);
+_putchar(48 + ((i * j) - (((i * j) / 100) * 100)) / 10);
 _putchar(48 + ((i * j) % 10));
 }
 else if (i * j >= 10)
