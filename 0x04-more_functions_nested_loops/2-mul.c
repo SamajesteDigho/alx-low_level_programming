5ec3f81baed3c00a43047e149f807a0ee4a61fbf
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-24 03:57:54 +0100

    Commiting

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * mul: Short description
+ * mul - Short description
  * @a: Function parameter
  * @b: Function parameter
- * Description : Fonction full description
  * Return: Return value description
  */
 int mul(int a, int b)
