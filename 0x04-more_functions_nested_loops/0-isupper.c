8ee86acbfab82910990004944ea90be1ed1776e1
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-24 03:59:23 +0100

    Commiting

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _isupper - Short description
- * @c : Function parameter
- * Return : Return value description
+ * @c: Function parameter
+ * Return: Return value description
  */
 int _isupper(int c)
 {
