522515ef2c6f46f9ebd21f69950ff922f08e2bac
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-24 01:06:56 +0100

    Commiting

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,24 @@ void more_numbers(void)
 int i, j;
 for (i = 0; i < 10; i++)
 {
-for (j =0; j < 15; j++)
+for (j = 0; j < 15; j++)
 {
-_putchar(48 + j);
+if (j >= 10)
+{
+_putchar(48 + (j / 10));
+}
+_putchar(48 + (j % 10));
 }
 _putchar('\n');
 }
-_putchar('\n');
+}
+
+void _putchar(int c)
+{
+putchar(c);
+}
+int main(void)
+{
+    more_numbers();
+    return (0);
 }
