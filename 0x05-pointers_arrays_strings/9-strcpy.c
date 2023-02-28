25410038955e3f47151c1663a3efb34b2894ef99
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-28 04:21:45 +0100

    Commiting

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - string length
+ * @dest : parameter
+ * @src : parameter
+ * Description: Describing the function
+ * Return: Array recopied
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i, j;
+i = strlen(src);
+j = strlen(dest);
+for (j = 0; j < i; j++)
+{
+dest[j] = src[j];
+}
+return dest;
+}
