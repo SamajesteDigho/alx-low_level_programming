4ca7d39e8670ae8f61bf592c0f2e516a2bde2eab
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-28 03:12:44 +0100

    Commitin

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - resetting
+ * @a : parameter
+ * @b : parameter
+ * Description: Describing the function
+ */
+void swap_int(int *a, int *b)
+{
+int c = (*a);
+(*a) = (*b);
+(*b) = c;
+}
