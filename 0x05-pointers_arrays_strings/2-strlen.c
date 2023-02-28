0a5026735237ef2357655556e91d0fcea8463a0b
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-28 03:20:10 +0100

    Commitin

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen - string length
+ * @s : parameter
+ * Description: Describing the function
+ * Return: Return number of caracters
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,11 +2,14 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <string.h>
 
 void _putchar(int c);
 
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
+
+int _strlen(char *s);
 
 #endif /* MAIN_H */
