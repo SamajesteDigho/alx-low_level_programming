82ca515770c9a91ffb30013426f9abb7a517da11
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-02-28 03:40:04 +0100

    Commiting

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - string length
+ * @s : parameter
+ * Description: Describing the function
+ */
+void rev_string(char *s)
+{
+char n;
+int i, j;
+i = strlen(s);
+for (j = 0; j < i / 2; j++)
+{
+n = s[i - j - 1];
+s[i - j - 1] = s[j];
+s[j] = n;
+}
+}
+}
