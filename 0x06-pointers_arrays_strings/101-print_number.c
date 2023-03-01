3da49f6e786caae1b45124c4506ed9ac01ac8414
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-01 18:53:44 +0100

    Commiting

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_number - resetting
+ * @n : parameter
+ * Description: Describing the function
+ */
+void print_number(int n)
+{
+
+}
+
+
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
+}
