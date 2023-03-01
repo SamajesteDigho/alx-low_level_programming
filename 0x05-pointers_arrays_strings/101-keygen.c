a1a4de0f249ed5a26f5018218e92e6555014b98e
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-01 04:10:09 +0100

    Commiting

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,7 +1,24 @@
 #include "main.h"
+#include <stdlib.h>
+#include <time.h>
 /**
- * print_array - string length
- * @a : parameter
- * @n : parameter
+ * main - string length
  * Description: Describing the function
+ * Return: Always return 0
  */
+
+int main(void)
+{
+int i, nb;
+char* pass;
+time_t t;
+srand((unsigned) time(&t));
+nb = (rand() % 20) + 5;
+pass = malloc(sizeof(char) * nb);
+for (i = 0; i < nb; i++)
+{
+pass[i] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ !.*-_+0123456789"[rand() % 43];
+}
+printf("%s\n", pass);
+return (0);
+}
