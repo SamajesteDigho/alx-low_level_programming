de1525fae918fccfae4f7d4804af104da799fd8c
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-01 03:32:02 +0100

    Commiting

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,12 +15,12 @@ n = 0;
 isn = 0;
 for (i = 0; i < l; i++)
 {
+if (s[i - 1] == '-')
+{
+isn++;
+}
 if (s[i] >= 48  && s[i] <= 57)
 {
-if (a == 0 && s[i - 1] == '-')
-{
-isn = 1;
-}
 n = (n * 10) + (s[i] - 48);
 b++;
 a = 1;
@@ -34,9 +34,33 @@ break;
 }
 }
 
-if (isn == 1)
+if (isn % 2 == 1)
 {
 n *= -1;
 }
 return (n);
+}
+
+
+int main(void)
+{
+    int nb;
+
+    nb = _atoi("98");
+    printf("%d\n", nb);
+    nb = _atoi("-402");
+    printf("%d\n", nb);
+    nb = _atoi("          ------++++++-----+++++--98");
+    printf("%d\n", nb);
+    nb = _atoi("214748364");
+    printf("%d\n", nb);
+    nb = _atoi("0");
+    printf("%d\n", nb);
+    nb = _atoi("Suite 402");
+    printf("%d\n", nb);
+    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+    printf("%d\n", nb);
+    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+    printf("%d\n", nb);
+    return (0);
 }
