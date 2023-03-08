503cd8daa07cedb3e1162a6f7cfdc8b019befbe6
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-08 11:38:54 +0100

    Commiting

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Function name
+ * @n: Parameter 1
+ * Description: 'Function full description'
+ * Return: Return 1 or 0
+ */
+int findFactor(int n, int pos)
+{
+if (pos == 1)
+{
+return (1);
+}
+if (n % pos == 0)
+{
+return (0);
+}
+else
+{
+return (findFactor(n, pos - 1));
+}
+}
+
+/**
+ * is_prime_number - Function name
+ * @n: Parameter 1
+ * Description: 'Function full description'
+ * Return: Return 1 or 0
+ */
+int is_prime_number(int n)
+{
+if (n <= 1)
+{
+return (0);
+}
+return (findFactor(n, n /2));
+}
