1507c36244707a0c1129e34b102d9b074fc69214
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-08 10:48:56 +0100

    Commiting

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,25 @@
 #include "main.h"
 /**
+ * search_sqrt - Function name
+ * @x: Parameter 1
+ * @index: Parameter 2
+ * Description: 'Function full description'
+ * Return: Return the square root
+ */
+int search_sqrt(int x, int index)
+{
+if (index == 0)
+{
+return (-1);
+}
+if (index * index == x)
+{
+return index;
+}
+return search_sqrt(x, index - 1);
+}
+
+/**
  * _sqrt_recursion - Function name
  * @n: Parameter 1
  * Description: 'Function full description'
@@ -7,8 +27,19 @@
  */
 int _sqrt_recursion(int n)
 {
-if (n == 0 || n == 1)
+int a;
+if (n < 0)
 {
-    re
+return (-1);
 }
+if (n == 0)
+{
+return (0);
+}
+if (n == 1)
+{
+return (1);
+}
+a = n / 2;
+return (search_sqrt(n, a));
 }
