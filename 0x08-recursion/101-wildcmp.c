531928fbb7f7cb7bc9da059118134bc945d28beb
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-08 14:08:12 +0100

    Commiting

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,9 +10,13 @@
  */
 int comparer(char *s1, char *s2, int p1, int p2)
 {
-    if (s1[p1] == '\0' && s2[p2] == '\0')
+    if (s1[p1] == '\0')
     {
+        if (s2[p2] == '\0')
+        {
         return (1);
+        }
+        return (0);
     }
     if (s1[p1] == s2[p2])
     {
@@ -58,4 +62,36 @@ int comparer(char *s1, char *s2, int p1, int p2)
 int wildcmp(char *s1, char *s2)
 {
 return (comparer(s1, s2, 0, 0));
+}
+
+
+int main(void)
+{
+    int r;
+
+    r = wildcmp("main.c", "*.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*a*i*n*.*c*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "main.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "ma********************************c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "***");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m.*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "**.*c");
+    printf("%d\n", r);
+    r = wildcmp("main-main.c", "ma*in.c");
+    printf("%d\n", r);
+    r = wildcmp("main", "main*d");
+    printf("%d\n", r);
+    r = wildcmp("abc", "*b");
+    printf("%d\n", r);
+    return (0);
 }
