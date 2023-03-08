db206f76cfc193fa39321181f4a92a284ee1ea8a
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-08 14:10:53 +0100

    Commiting

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,42 +10,42 @@
  */
 int comparer(char *s1, char *s2, int p1, int p2)
 {
-    if (s1[p1] == '\0' && s2[p2] == '\0')
-    {
-        return (1);
-    }
-    if (s1[p1] == s2[p2])
-    {
-        return (comparer(s1, s2, p1 + 1, p2 + 1));
-    }
-    if (s2[p2] == '*')
-    {
-    /*printf("* Detected\n");*/
-        if (s2[p2 + 1] == '*')
-        {
-            return (comparer(s1, s2, p1, p2 + 1));
-        }
-        else
-        {
-            if (s1[p1 + 1] == s2[p2 + 1])
-            {
-                return (comparer(s1, s2, p1 + 1, p2 + 1));
-            }
-            else if(s1[p1 + 1] == '\0')
-            {
-                if (s2[p2 + 1] != '\0')
-                {
-                    return (1);
-                }
-                return (comparer(s1, s2, p1, p2 + 1));
-            }
-            else
-            {
-                return (comparer(s1, s2, p1 + 1, p2));
-            }
-        }
-    }
-    return (0);
+if (s1[p1] == '\0' && s2[p2] == '\0')
+{
+return (1);
+}
+if (s1[p1] == s2[p2])
+{
+return (comparer(s1, s2, p1 + 1, p2 + 1));
+}
+if (s2[p2] == '*')
+{
+/*printf("* Detected\n");*/
+if (s2[p2 + 1] == '*')
+{
+return (comparer(s1, s2, p1, p2 + 1));
+}
+else
+{
+if (s1[p1 + 1] == s2[p2 + 1])
+{
+return (comparer(s1, s2, p1 + 1, p2 + 1));
+}
+else if(s1[p1 + 1] == '\0')
+{
+if (s2[p2 + 1] != '\0')
+{
+return (1);
+}
+return (comparer(s1, s2, p1, p2 + 1));
+}
+else
+{
+return (comparer(s1, s2, p1 + 1, p2));
+}
+}
+}
+return (0);
 }
 
 /**
