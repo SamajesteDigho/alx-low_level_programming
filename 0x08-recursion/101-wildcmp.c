1501783229cc175f8d6e81e24a7cf1987b6587fd
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-08 13:31:49 +0100

    Commiting

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * comparer - Function name
+ * @s1: Parameter 1
+ * @s2: Parameter 2
+ * @p1: Parameter 1
+ * @p2: Parameter 2
+ * Description: 'Function full description'
+ * Return: Return 0 or 1
+ */
+int comparer(char *s1, char *s2, int p1, int p2)
+{
+    if ((s1[p1] == '\n' && s2[p2] == '\n') || (s1[p1] == '\0' && s2[p2] == '\0'))
+    {
+        return (1);
+    }
+    if (s1[p1] == s2[p2])
+    {
+        return (comparer(s1, s2, p1 + 1, p2 + 1));
+    }
+    if (s2[p2] == '*')
+    {
+    /*printf("* Detected\n");*/
+        if (s2[p2 + 1] == '*')
+        {
+            return comparer(s1, s2, p1, p2 + 1);
+        }
+        else
+        {
+
+            if (s1[p1 + 1] == s2[p2 + 1])
+            {
+                return (comparer(s1, s2, p1 + 1, p2 + 1));
+            }
+            else
+            {
+                return (comparer(s1, s2, p1 + 1, p2));
+            }
+        }
+    }
+    return (0);
+}
+
+/**
+ * wildcmp - Function name
+ * @s1: Parameter 1
+ * @s2: Parameter 2
+ * Description: 'Function full description'
+ * Return: Return 0 or 1
+ */
+int wildcmp(char *s1, char *s2)
+{
+return comparer(s1, s2, 0, 0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -18,5 +18,6 @@ int _pow_recursion(int x, int y);
 
 int is_palindrome(char *s);
 
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
