35f41509296037710ce5b32d42bbe431260296a9
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-08 14:35:23 +0100

    Commiting

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -44,15 +44,3 @@ break;
 }
 return (p);
 }
-
-
-int main(void)
-{
-	char *s = "First, solve the problem. Then, write the code.";
-	char *f = "";
-	char *t;
-
-	t = _strstr(s, f);
-	printf("%s\n", (t == NULL ? "nil" : t));
-	return (0);
-}
