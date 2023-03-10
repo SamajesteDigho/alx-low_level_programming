f603ccc1d5329dce32381ec314cfcd66911d45c4
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-10 15:42:00 +0100

    Commiting

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - Entry point
+ * @argc: Argument 1
+ * @argv: Argument 2
+ * Description: Main function description
+ * Return: Always return 0.
+ */
 int main(int argc, char **argv)
 {
 printf("%s\n", argv[argc - argc]);
