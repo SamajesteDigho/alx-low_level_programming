7f4788f4ce2379348dea05599f12c2b61b27b46a
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-13 13:01:11 +0100

    Commiting

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * create_array - Function name
+ * @size: Parameter 1
+ * @c: Parameter 2
+ * Description: Here the description
+ * Return: Returns pointer to a char
+ */
+char *create_array(unsigned int size, char c)
+{
+char *res;
+if (size == 0)
+{
+return (NULL);
+}
+res = malloc(sizeof(char) * size);
+res[0] = c;
+return (res);
+}
