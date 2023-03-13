b1fe88d41ae3811c60a710267ac623d378550bf3
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-13 05:28:29 +0100

    Commiting

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 /**
- *
+ * main - Entry point
+ * @argc: Parameter 1
+ * @argv: Parameter 2
+ * Description: Program description
+ * Return: Always return 0.
  */
 int main(int argc, char **argv)
 {
+int amt, nbc, tnc;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+amt = atoi(argv[1]);
+tnc = 0;
+nbc = 0;
+if (amt < 0)
+{
+printf("0\n");
+return (0);
+}
+/* Number of coins of 25 cents */
+nbc = max_nb_coins(amt, 25);
+tnc += nbc;
+amt -= nbc * 25;
+
+/* Number of coins of 10 cents */
+nbc = max_nb_coins(amt, 10);
+tnc += nbc;
+amt -= nbc * 10;
+
+/* Number of coins of 5 cents */
+nbc = max_nb_coins(amt, 5);
+tnc += nbc;
+amt -= nbc * 5;
+
+/* Number of coins of 2 cents */
+nbc = max_nb_coins(amt, 2);
+tnc += nbc;
+amt -= nbc * 2;
+
+/* Number of coins of 2 cents */
+nbc = max_nb_coins(amt, 1);
+tnc += nbc;
+amt -= nbc * 1;
+
+printf("%d\n", tnc);
+
+return (0);
+}
+
+/**
+ * max_nb_coins - Breaking coin function
+ * @amt: Parameter 1
+ * @coin: Parameter 2
+ * Description: Program description
+ * Return: Always return 0.
+ */
+int max_nb_coins(int amt, int coin)
+{
+int a;
+a = amt / coin;
+return a;
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+int max_nb_coins(int amt, int coin);
 
 #endif /* MAIN_H */
