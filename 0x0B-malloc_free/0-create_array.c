55cb325fcb8375f1491dd339fa5e8827168f6e71
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-13 14:13:50 +0100

    Commiting

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,13 +8,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
-char res[size];
+char res;
 unsigned int i;
 if (size == 0)
 {
 return (NULL);
 }
-// res = malloc(size * sizeof(char));
+res = malloc(size * sizeof(char));
 for (i = 0; i < size; i++)
 {
 res[i] = c;
