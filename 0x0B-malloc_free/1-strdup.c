0daaef6f838368c3e0c80846a52f0735dc109776
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-13 14:46:13 +0100

    Commiting

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strdup - Function name
+ * @str: Parameter 1
+ * Description: Here the description
+ * Return: Returns pointer to a char
+ */
+char *_strdup(char *str)
+{
+int size, i;
+char *res;
+size = strlen(str);
+res = malloc(size * sizeof(char));
+for (i = 0; i < size; i++)
+{
+res[i] = str[i];
+}
+return (res);
+}
