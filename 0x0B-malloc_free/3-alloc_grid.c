540464a84f401dbe26862d95766164f6b89cf3ba
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-14 09:06:54 +0100

    Commiting

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,23 +16,23 @@ if (width <= 0 || height <= 0)
 return (NULL);
 }
 
-mat = (int **) malloc(height * sizeof(int *));
+mat = (int **) malloc(width * sizeof(int *));
 if (mat == NULL)
 {
 return (NULL);
 }
-for (i = 0; i < height; i++)
+for (i = 0; i < width; i++)
 {
-mat[i] = (int *) malloc(width * sizeof(int));
+mat[i] = (int *) malloc(height * sizeof(int));
 if (mat[i] == NULL)
 {
 return (NULL);
 }
 }
 
-for (i = 0; i < height; i++)
+for (i = 0; i < width; i++)
 {
-for (j = 0; j < width; j++)
+for (j = 0; j < height; j++)
 {
 mat[i][j] = 0;
 }
