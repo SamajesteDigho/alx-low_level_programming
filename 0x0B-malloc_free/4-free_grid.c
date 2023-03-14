5ef7c04a99affc6bbaffe84ede7c1d6cc24cf2f4
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-14 08:23:59 +0100

    Commiting

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * free_grid - Function name
+ * @grid: Parameter 1
+ * @height: Parameter 2
+ * Description: Here the description
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+}
