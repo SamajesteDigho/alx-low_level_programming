2b928dc0a1526239a53bcfd8b54ff0403aa82994
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-14 11:05:26 +0100

    Commiting

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include "main.h"
+/**
+ * argstostr - Function name
+ * @ac: Parameter 1
+ * @av: Parameter 2
+ * Description: Here the description
+ * Return: Characters
+ */
+char *argstostr(int ac, char **av)
+{
+char *res;
+int i, j, k, s_size, t_size;
+if (ac == 0 || av == NULL)
+{
+return (NULL);
+}
+t_size = 0;
+for (i = 1; i < ac; i++)
+{
+t_size += strlen(av[i]);
+}
+res = (char *) malloc(t_size * sizeof(char) + t_size);
+i = 0;
+for (j = 1; j < ac; j++)
+{
+s_size = strlen(av[j]);
+for (k = 0; k < s_size; k++)
+{
+res[i++] = av[j][k];
+}
+res[i++] = '\n';
+}
+return (res);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * strtow - Function name
+ * @str: Parameter 1
+ * Description: Here the description
+  * Return: Characters
+ */
+char **strtow(char *str)
+{
+}
