6f519a9e0f279266799f5dce3d5ede63db89e5e3
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-20 10:08:04 +0100

    Commiting

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,8 +3,8 @@
 /**
  * print_dog - Function name
  * @d: Only parameter
+ * Description: Here the description
  */
-
 void print_dog(struct dog *d)
 {
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
