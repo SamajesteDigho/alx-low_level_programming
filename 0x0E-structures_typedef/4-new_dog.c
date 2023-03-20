be15682deeed314d1acf05bd2a20af35bdf7c6c1
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-20 10:10:51 +0100

    Commiting

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,6 +14,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
 d = malloc(sizeof(dog_t));
+if (d == NULL)
+{
+return (NULL);
+}
 d->name = name;
 d->age = age;
 d->owner = owner;
