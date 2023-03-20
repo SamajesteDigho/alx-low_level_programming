a68851ab49f311394292df685e13cfd25543136d
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-20 10:13:57 +0100

    Commiting

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-
 /**
  * free_dog - check the code
  * @d: Parameter 1
  * Description: Here the code description
  */
-
 void free_dog(dog_t *d)
 {
-free(d->name);
-free(&(*d).age);
-free(d->owner);
+free(d);
 }
