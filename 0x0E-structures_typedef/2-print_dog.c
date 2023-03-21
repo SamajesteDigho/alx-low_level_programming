1cccaa5ce0373256fb4b3e21f61c46ba0b475915
Le Digho <74926093+SamajesteDigho@users.noreply.github.com>
2023-03-21 12:10:21 +0100

    Update 2-print_dog.c
    Commiting

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,6 +9,6 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
-printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+printf("Name: %p\nAge: %f\nOwner: %p\n", d->name, d->age, d->owner);
 }
 }
