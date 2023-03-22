6807c83ce51d8dc1a47c363ba5df2dbd393de682
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-22 13:08:04 +0100

    Commiting

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,14 +3,18 @@
  * array_iterator - Function name
  * @array: Parameter 1
  * @size: parameter 2
+ * @action: parameter 3
  * Description: Describing the function
  * Return: Returns Nothing
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
+if (action)
+{
 for (i = 0; i < size; i++)
 {
 action(array[i]);
+}
 }
 }
