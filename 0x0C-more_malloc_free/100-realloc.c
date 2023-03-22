db9708f9d6bca3d35ceb10f6eb25b0cd8d3b56e1
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-22 12:34:03 +0100

    Commiting

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,5 @@
-#include <string.h>
 #include "main.h"
+#include <string.h>
 /**
  * _realloc - Function name
  * @ptr: Parameter 1
@@ -10,25 +10,26 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-unsigned int i;
 if (ptr)
 {
 if (new_size == 0)
 {
 free(ptr);
+ptr = NULL;
 return (NULL);
 }
-if (old_size > new_size)
+if (new_size == old_size)
 {
-for (i = 0; i < new_size; i++)
-{
-ptr[i] = ptr[i];
+return (ptr);
 }
-ptr[i] = '\0';
+if (old_size < new_size)
+{
+ptr = malloc(new_size);
 return (ptr);
 }
 else
 {
+ptr = malloc(new_size);
 return (ptr);
 }
 }
