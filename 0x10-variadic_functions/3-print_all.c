5e01ec8757bf360cead7cb9878b6fac541b363d2
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-28 12:56:53 +0100

    Commiting

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,75 +1,54 @@
 #include "variadic_functions.h"
-
 /**
  * print_all - Function name
  * @format: Arguments format
- * @...: Unknown list of parameters
- * Description: This function sums all its parameters
  */
 void print_all(const char * const format, ...)
 {
 va_list args;
 int walker;
-char *current;
 if (format)
 {
 walker = 0;
 va_start(args, format);
 while (format[walker + 1] != '\0')
 {
-switch (format[walker])
-{
-case 'c':
-printf("%c", va_arg(args, int));
+_printer(format[walker], args);
 printf(", ");
-break;
-case 'i':
-printf("%d", va_arg(args, int));
-printf(", ");
-break;
-case 'f':
-printf("%f", va_arg(args, double));
-printf(", ");
-break;
-case 's':
-current = va_arg(args, char *);
-printf("%s", get_string(current));
-printf(", ");
-break;
-}
 walker++;
 }
-switch (format[walker])
-{
-case 'c' :
-printf("%c", va_arg(args, int));
-break;
-case 'i' :
-printf("%d", va_arg(args, int));
-break;
-case 'f' :
-printf("%f", va_arg(args, double));
-break;
-case 's' :
-current = va_arg(args, char *);
-printf("%s", get_string(current));
-break;
-}
+_printer(format[walker], args);
+va_end(args);
 }
 printf("\n");
 }
-
 /**
  * get_string - Function name
  * @value: Arguments format
- * Description: This function returns a string accordingly
  * Return: Returns a string
  */
 char *get_string(char *value)
 {
 if (value == NULL)
-{
 return ("(nil)");
-}
 return (value);
+}
+/**
+ * _printer - Function name
+ * @c: Arguments format
+ * @args: Arguments format
+ */
+void _printer(char c, va_list args)
+{
+switch (c)
+{
+case 'c': printf("%c", va_arg(args, int));
+break;
+case 'i': printf("%d", va_arg(args, int));
+break;
+case 'f': printf("%f", va_arg(args, double));
+break;
+case 's': printf("%s", get_string(va_arg(args, char *)));
+break;
+}
 }
