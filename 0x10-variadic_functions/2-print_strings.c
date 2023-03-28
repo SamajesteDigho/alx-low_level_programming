8f6b7f87ba8cc96295c84a825a0752b357523cc2
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-28 11:08:02 +0100

    Commiting

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,4 +8,24 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+va_list args;
+unsigned int walker;
+if (n != 0)
+{
+va_start(args, n);
+for (walker = 0; walker < n - 1; walker++)
+{
+printf("%s", va_arg(args, char *));
+if (separator)
+{
+printf("%s", separator);
+}
+}
+printf("%s\n", va_arg(args, char *));
+va_end(args);
+}
+else
+{
+printf("\n");
+}
 }
