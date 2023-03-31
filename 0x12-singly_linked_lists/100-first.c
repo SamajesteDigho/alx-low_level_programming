a12799337e475e33dd0b3b38b398cf4cf475e4db
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-31 17:35:02 +0100

    Commiting

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
  * print_intro - Function name
+ *
  * Description: Function that adds a new node at the beginning of a list
  */
 void __attribute__((constructor)) print_intro()
