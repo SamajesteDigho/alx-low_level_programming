3cb8aa152ddf66ca0de7235f6be6ea117d167695
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-31 12:15:36 +0100

    Commiting

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,8 +1,33 @@
 #include "lists.h"
 /**
- * add_node - Function name
+ * add_node_end - Function name
  * @head: Parameter 1
  * @str: Parameter 2
  * Description: Function that adds a new node at the beginning of a list
  * Return: Returns the size of the linked list
  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *new, *tmp;
+new = malloc(sizeof(list_t));
+if (new)
+{
+new->str = strdup(str);
+new->len = strlen(str);
+new->next = NULL;
+if (*head == NULL) {
+(*head) = new;
+}
+else
+{
+tmp = (*head);
+while (tmp->next != NULL)
+{
+tmp = tmp->next;
+}
+tmp->next = new;
+return (*head);
+}
+}
+return (NULL);
+}
