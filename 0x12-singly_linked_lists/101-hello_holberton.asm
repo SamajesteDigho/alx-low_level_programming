c01295e18e878143daca6139c2dd4d4e262bde44
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-31 21:01:07 +0100

    Commiting

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,6 @@
 section .data
-hello db "Hello, Holberton", 0x0A   ; null-terminated string with new line character
-
+hello db "Hello, Holberton", 0x0A
 section .text
 global _start
 
-_start:
-; write the string to stdout
-mov eax, 4      ; syscall number for write
-mov ebx, 1      ; file descriptor for stdout
-mov ecx, hello  ; pointer to the message to print
-mov edx, 16     ; message length in bytes
-int 0x80        ; invoke the system call
 
