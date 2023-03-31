be179e4a0defb2b65adf0819dd925975b65b3ff4
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-03-31 13:15:21 +0100

    Commiting

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * print_intro - Function name
+ * Description: Function that adds a new node at the beginning of a list
+ */
+void __attribute__((constructor)) print_intro() {
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
