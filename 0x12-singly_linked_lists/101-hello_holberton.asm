0b958088c914b0128b2eb8b683c86a2626f2fa81
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-04-01 05:07:00 +0100

    Commiting

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .data
+    hello db "Hello, Holberton", 0x0A   ; null-terminated string with new line character
+
+section .text
+    global _start
+
+_start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, hello
+    mov edx, 16
+    int 0x80
+
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
