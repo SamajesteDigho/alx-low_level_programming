4bdd2ade3882367512297f68c89f0ff522a98845
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-04-03 11:59:09 +0100

    Commiting

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,6 +9,18 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
-printf("Name: %p\nAge: %p\nOwner: %p\n", d->name, d->age, d->owner);
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
+}
+
+
+int main(void)
+{
+    struct dog my_dog;
+
+    my_dog.name = "Poppy";
+    my_dog.age = 3.5;
+    my_dog.owner = "Bob";
+    print_dog(&my_dog);
+    return (0);
 }
