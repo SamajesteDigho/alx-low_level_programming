190b26024bd20714c249b8d1b2b0ef3889672803
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-04-03 11:32:13 +0100

    Commiting

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: Number of parameters
+ * @argv: Vector of arguments
+ * Description: Multiply 2 parameters passed to the file
+ * Return: Always 0.
+*/
+int main(int argc, char **argv)
+{
+int a, b;
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+if (a == 0 || b == 0)
+{
+printf("Error\n");
+exit(98);
+}
+printf("%d\n", a * b);
+return (0);
+}
