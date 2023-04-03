0f694c923b8aecb8df040c3542db438ae18b74df
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-04-03 11:48:39 +0100

    Commiting

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -16,11 +16,39 @@ exit(98);
 }
 a = atoi(argv[1]);
 b = atoi(argv[2]);
-if (a == 0 || b == 0)
+if (check_only_numbers(argv[1]) == -1)
 {
 printf("Error\n");
 exit(98);
 }
+if (check_only_numbers(argv[2]) == -1)
+{
+printf("Error\n");
+exit(98);
+}
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+
 printf("%li\n", a *b);
 return (0);
+}
+
+/**
+ * check_only_numbers - Function Name
+ * @str: Number of parameters
+ * Description: Check if a string is essentially a number
+ * Return: Always 1 or -1.
+*/
+int check_only_numbers(char* str)
+{
+unsigned int size, i;
+size = strlen(str);
+for (i = 0; i < size; i++)
+{
+if (str[i] < '0' && str[i] > '9')
+{
+return (-1);
+}
+return (1);
+}
 }
