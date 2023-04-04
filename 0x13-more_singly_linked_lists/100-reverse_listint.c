07943218f397fef19c0161d9d7c38ceed6325f47
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-04-04 13:50:42 +0100

    Commiting

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * reverse_listint - Function name
+ * @head: Parameter 1, List header
+ * Description: Function that returns sum of all nodes
+ * Return: The value of head of reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *tmp, *mov;
+tmp = (*head);
+mov = tmp->next;
+tmp->next = NULL;
+while (mov != NULL)
+{
+(*head) = mov;
+mov = mov->next;
+(*head)->next = tmp;
+tmp = (*head);
+}
+return (*head);
+}
