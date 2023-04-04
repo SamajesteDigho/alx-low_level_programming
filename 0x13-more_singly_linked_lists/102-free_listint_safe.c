7e7c9ce70bd23668c2a3c2f0e1173e58c5f0ad97
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-04-04 14:41:29 +0100

    Commiting

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * free_listint_safe - Function name
+ * @h: Parameter 1, List header
+ * Description: Function that returns sum of all nodes
+ * Return: The size of freed list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+listint_t *tmp, *del;
+size_t size;
+size = 0;
+tmp = (*h);
+while (tmp != NULL)
+{
+del = tmp;
+tmp = tmp->next;
+del->next = NULL;
+free(del);
+del = NULL;
+size++;
+}
+(*h) = NULL;
+return (size);
+}
