6a03332bedb6d21fd4958b504be3a2d067637b23
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-04-04 15:32:51 +0100

    Commiting

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,69 @@
+#include "lists.h"
+/**
+ * find_listint_loop - Function name
+ * @head: Parameter 1, List header
+ * Description: Function that returns sum of all nodes
+ * Return: The size of freed list
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+listint_t *tmp;
+tmp = head;
+while (tmp != NULL && tmp->n != -1)
+{
+tmp->n = tmp->n * -1;
+tmp = tmp->next;
+}
+if (tmp == NULL || tmp->n == -1)
+{
+return (NULL);
+}
+tmp->n = tmp->n * -1;
+return (tmp);
+}
+
+
+
+int main(void)
+{
+    listint_t *head;
+    listint_t *head2;
+    listint_t *node;
+
+    head2 = NULL;
+    add_nodeint(&head2, 0);
+    add_nodeint(&head2, 1);
+    add_nodeint(&head2, 2);
+    add_nodeint(&head2, 3);
+    add_nodeint(&head2, 4);
+    add_nodeint(&head2, 98);
+    add_nodeint(&head2, 402);
+    add_nodeint(&head2, 1024);
+    print_listint_safe(head2);
+    node = find_listint_loop(head2);
+    if (node != NULL)
+    {
+        printf("Loop starts at [%p] %d\n", (void *)node, node->n);
+    }
+    free_listint_safe(&head2);
+    head = NULL;
+    node = add_nodeint(&head, 0);
+    add_nodeint(&head, 1);
+    add_nodeint(&head, 2);
+    add_nodeint(&head, 3);
+    add_nodeint(&head, 4);
+    add_nodeint(&head, 5);
+    add_nodeint(&head, 6);
+    node->next = add_nodeint(&head, 7);
+    add_nodeint(&head, 98);
+    add_nodeint(&head, 402);
+    add_nodeint(&head, 1024);
+    print_listint_safe(head);
+    node = find_listint_loop(head);
+    if (node != NULL)
+    {
+        printf("Loop starts at [%p] %d\n", (void *)node, node->n);
+    }
+    free_listint_safe(&head);
+    return (0);
+}
