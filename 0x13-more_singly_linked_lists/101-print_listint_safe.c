dea3a4db7bed3cbef77fbbbffe80cd70dfdd8cf3
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-04-04 14:31:25 +0100

    Commiting

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,12 +10,14 @@ size_t print_listint_safe(const listint_t *head)
 listint_t *tmp;
 size_t size;
 size = 1;
-while (head != NULL && head->n != -1)
+printf("%d\n", head->n);
+tmp = head->next;
+while (tmp != NULL && tmp->n != -1)
 {
 size++;
-printf("%d\n", head->n);
-head->n = -1;
-head = head->next;
+printf("%d\n", tmp->n);
+tmp->n = -1;
+tmp = tmp->next;
 }
 return (size);
 }
