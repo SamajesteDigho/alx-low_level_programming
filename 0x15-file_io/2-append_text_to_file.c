adfd3a059ea78615e959c6369ddd380ddf8c1ef9
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-04-13 10:17:11 +0100

    Commiting

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,17 +29,3 @@ return (1);
 }
 return (-1);
 }
-
-int main(int ac, char **av)
-{
-    int res;
-
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = append_text_to_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
-}
