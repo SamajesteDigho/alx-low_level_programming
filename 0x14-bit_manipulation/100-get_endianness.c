4d194bb370ceb7aec3a0c9e34fadd73315cf5cbf
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-04-13 08:28:58 +0100

    Commiting

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -20,19 +20,3 @@ else
 return (0);
 }
 }
-
-int main(void)
-{
-    int n;
-
-    n = get_endianness();
-    if (n != 0)
-    {
-        printf("Little Endian\n");
-    }
-    else
-    {
-        printf("Big Endian\n");
-    }
-    return (0);
-}
