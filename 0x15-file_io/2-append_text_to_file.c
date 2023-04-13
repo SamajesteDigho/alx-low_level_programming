8229ff3e11d66d0fb087cf88d681a3c82fd9efa9
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-04-13 10:16:43 +0100

    Commiting

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * append_text_to_file - Function name
+ * @filename: Path to the file
+ * @letters: The number of letters to read
+ * Description: This function reads some characters
+ * Return: The number of written characters
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+FILE *fp;
+if (filename == NULL)
+{
+return (-1);
+}
+if (access(filename, F_OK) == 0)
+{
+if (text_content == NULL)
+{
+return (-1);
+}
+return (-1);
+}
+fp = fopen(filename, "a+");
+if (fp)
+{
+fputs(text_content, fp);
+return (1);
+}
+return (-1);
+}
+
+int main(int ac, char **av)
+{
+    int res;
+
+    if (ac != 3)
+    {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
+    }
+    res = append_text_to_file(av[1], av[2]);
+    printf("-> %i)\n", res);
+    return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 size_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
 #endif /* MAIN_H */
