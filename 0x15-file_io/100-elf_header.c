76113f8667c2de693475c91ad8b7229c6588026d
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2023-12-23 19:32:16 +0800

    Commiting

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * check_elf_file - Check is an elf file
+ * check_elf - Check is an elf file
  * @magic: Magic number array
- * Description: This function collects the magic number array and return 0=false and 1=true
+ * Description: This function collects the magic array and return 0 or 1
  * Return: Always return 0 or 1
 */
-int check_elf_file(unsigned char* magic)
+int check_elf(unsigned char *magic)
 {
 if (magic[0] == 0x7F && magic[1] == 'E' && magic[2] == 'L' && magic[3] == 'F')
 {
@@ -15,12 +15,12 @@ return (0);
 }
 
 /**
- * get_osabi - Get the OSABI Real Name
+ * elf_osabi - Get the OSABI Real Name
  * @code: OSABI Code
  * Description: This function collects a code return the corresponding OSABI
- * Return: The OSABI name 
+ * Return: The OSABI name
 */
-char* get_elf_osabi(unsigned char code)
+char *elf_osabi(unsigned char code)
 {
 switch (code)
 {
@@ -35,12 +35,12 @@ default: return ("<unknown: 53>");
 }
 
 /**
- * get_elf_type - Get the Type Name
+ * elf_type - Get the Type Name
  * @type: OSABI Code
  * Description: This function collects a code return the corresponding Type
- * Return: The Type name 
+ * Return: The Type name
 */
-char* get_elf_type(Elf64_Half type)
+char *elf_type(Elf64_Half type)
 {
 switch (type)
 {
@@ -53,6 +53,37 @@ default: return ("Unknown");
 }
 }
 
+/**
+ * elf_class - Get the Type Name
+ * @class: OSABI Code
+ * Description: This function collects a code return the corresponding Type
+ * Return: The Type name
+*/
+char *elf_class(unsigned char class)
+{
+switch (class)
+{
+case ELFCLASS32: return ("ELF32");
+case ELFCLASS64: return ("ELF64");
+default: return ("Unknown");
+}
+}
+
+/**
+ * elf_data - Get the Type Name
+ * @data: OSABI Code
+ * Description: This function collects a code return the corresponding Type
+ * Return: The Type name
+*/
+char *elf_data(unsigned int data)
+{
+switch (data)
+{
+case ELFDATA2LSB: return ("2's complement, little endian");
+case ELFDATA2MSB: return ("2's complement, big endian");
+default: return ("2's complement, big endian");
+}
+}
 
 /**
  * main - Entry point
@@ -64,7 +95,11 @@ default: return ("Unknown");
 int main(int argc, char **argv)
 {
 int elf;
-Elf64_Ehdr header;
+Elf64_Ehdr h;
+unsigned char class;
+unsigned int data;
+unsigned char osabi;
+int osabi_version;
 int i;
 if (argc != 2)
 {
@@ -78,27 +113,31 @@ fprintf(stderr, "The file could not be found.\n");
 close(elf);
 exit(98);
 }
-read(elf, &header, sizeof(Elf64_Ehdr));
-if (check_elf_file((&header)->e_ident) == 0)
+read(elf, &h, sizeof(Elf64_Ehdr));
+if (check_elf((&h)->e_ident) == 0)
 {
 fprintf(stderr, "The parsed file is not an ELF file.\n");
 exit(98);
 }
+class = (&h)->e_ident[EI_CLASS];
+data = (&h)->e_ident[EI_DATA];
+osabi = (&h)->e_ident[EI_OSABI];
+osabi_version = (&h)->e_ident[EI_ABIVERSION];
 printf("ELF Header:\n");
 printf("  Magic:   ");
 for (i = 0; i < EI_NIDENT - 1; ++i)
 {
-printf("%02x ", (&header)->e_ident[i]);
+printf("%02x ", (&h)->e_ident[i]);
 }
-printf("%02x\n", (&header)->e_ident[i]);
-printf("  Class:                             %s\n", (&header)->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-printf("  Data:                              %s\n", (&header)->e_ident[EI_DATA] ? "2's complement, little endian" : "2's complement, big endian");
-printf("  Version:                           %d", (&header)->e_ident[EI_VERSION]);
-printf("%s", (&header)->e_version == EV_CURRENT ? " (current)\n" : "");
-printf("  OS/ABI:                            %s\n", get_elf_osabi((&header)->e_ident[EI_OSABI]));
-printf("  ABI Version:                       %d\n", (&header)->e_ident[EI_ABIVERSION]);
-printf("  Type:                              %s\n", get_elf_type((&header)->e_type));
-printf("  Entry point address:               0x%x\n", (unsigned int) (&header)->e_entry);
+printf("%02x\n", (&h)->e_ident[i]);
+printf("  Class:                             %s\n", elf_class(class));
+printf("  Data:                              %s\n", elf_data(data));
+printf("  Version:                           %d", (&h)->e_ident[EI_VERSION]);
+printf("%s\n", (&h)->e_version == EV_CURRENT ? " (current)" : "");
+printf("  OS/ABI:                            %s\n", elf_osabi(osabi));
+printf("  ABI Version:                       %d\n", osabi_version);
+printf("  Type:                              %s\n", elf_type((&h)->e_type));
+printf("  Entry point address:               0x%x\n", (int) (&h)->e_entry);
 close(elf);
 return(0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,8 +13,10 @@
 size_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int check_elf_file(unsigned char* magic);
-char* get_elf_osabi(unsigned char code);
-char* get_elf_type(Elf64_Half type);
+int check_elf(unsigned char *magic);
+char *elf_osabi(unsigned char code);
+char *elf_type(Elf64_Half type);
+char *elf_class(unsigned char class);
+char *elf_data(unsigned int type);
 
 #endif /* MAIN_H */
