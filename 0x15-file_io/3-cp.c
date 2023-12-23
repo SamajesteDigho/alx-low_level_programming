4dc12affec10b7bcf226c4ea95d7e7f060ff9fee
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2023-12-23 16:33:22 +0800

    Commiting

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,13 +13,13 @@ char buffer[1024];
 size_t readLength;
 if (argc != 3)
 {
-printf("Usage: cp file_from file_to\n");
+fprintf(stderr, "Usage: cp file_from file_to\n");
 exit(97);
 }
 from = fopen(argv[1], "r");
 if (from == NULL && (access(argv[1], F_OK) != 0))
 {
-printf("Error: Can't read from file %s\n", argv[1]);
+fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 if (access(argv[2], F_OK) == 0) {
@@ -31,7 +31,7 @@ chmod(argv[2], S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 }
 if (to == NULL)
 {
-printf("Error: Can't write to %s\n", argv[2]);
+fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 while ((readLength = fread(buffer, 1, 1024, from)) > 0)
@@ -40,12 +40,12 @@ fwrite(buffer, 1, readLength, to);
 }
 if (fclose(to) == -1)
 {
-printf("Error: Can't close fd  %d\n", errno);
+fprintf(stderr, "Error: Can't close fd  %d\n", errno);
 exit(100);
 }
 if (fclose(from) == -1)
 {
-printf("Error: Can't close fd %d\n", errno);
+fprintf(stderr, "Error: Can't close fd %d\n", errno);
 exit(100);
 }
 return (0);
