eee36e3e6690938507c60f9f74c9fc255ed482e8
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-01-26 09:55:38 +0800

    Commiting

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -6,11 +6,10 @@
  * @argc: Number of arguments
  * @argv: List of arguments
  * Description: Generate key and print
- * Return: 1 if succeed or exit
+ * Return: 0 if succeed or exit
 */
 int main(int argc, char **argv)
 {
-/*char a[10], c[10], d;*/
 int str_len, i, j;
 char *name, *key;
 if (argc < 2)
@@ -27,7 +26,7 @@ for(i = str_len - 1; i >= 0; i--)
 key[j] = name[i] + i;
 j++;
 }
-printf("%s\n", key);
+printf("%s", key);
 free(key);
 return (0);
 }
