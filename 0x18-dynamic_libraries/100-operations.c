bbd53b9d12acc56cb8fc486fb78c40ddeca1edc8
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-01-30 03:32:00 +0800

    Commiting

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,58 @@
+#include "main.h"
+/**
+ * add - Function
+ * @a: Param 1
+ * @b: Param 2
+ * Description: Permits to operate the of 2 numbers
+ * Return: Result of the operation
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * sub - Function
+ * @a: Param 1
+ * @b: Param 2
+ * Description: Permits to operate the of 2 numbers
+ * Return: Result of the operation
+*/
+int sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * mul - Function
+ * @a: Param 1
+ * @b: Param 2
+ * Description: Permits to operate the of 2 numbers
+ * Return: Result of the operation
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * div - Function
+ * @a: Param 1
+ * @b: Param 2
+ * Description: Permits to operate the of 2 numbers
+ * Return: Result of the operation
+*/
+int div(int a, int b)
+{
+if (b == 0)
+return (0);
+return (a / b);
+}
+/**
+ * mod - Function
+ * @a: Param 1
+ * @b: Param 2
+ * Description: Permits to operate the of 2 numbers
+ * Return: Result of the operation
+*/
+int mod(int a, int b)
+{
+return (a % b);
+}
