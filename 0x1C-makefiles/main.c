f565e6555caf62422d50bc74af5358df096adbc9
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-05 14:49:10 +0800

    Commiting

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+int main(int argc, char* argv)
+{
+    print_rubbish();
+}
---
+++ 0x1C-makefiles/main.h
@@ -0,0 +1,8 @@
+#ifndef SCHOOL
+#define SCHOOL
+
+#include <stdio.h>
+
+void print_rubbish(void);
+
+#endif /* SCHOOL */
