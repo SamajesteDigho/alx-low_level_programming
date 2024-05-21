8388786bdeec7c49992f61feeeb766d3faabfa97
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-21 08:44:09 +0800

    Exo 1

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+/**
+ * linear_search - Linear Search
+ * @array: The array of values
+ * @size: Size of the array
+ * @value: The value to be searched
+ * Description: Find and return the index of the first occurence of the value.
+ * Return: Index or -1 if not found
+*/
+int linear_search(int *array, size_t size, int value)
+{
+size_t i;
+if (array == NULL || size == 0)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+if (array[i] == value)
+{
+return (i);
+}
+}
+return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,3 @@
+## 0x1E-search_algorithms
+
+Here the readme for 0x1E-search_algorithms
