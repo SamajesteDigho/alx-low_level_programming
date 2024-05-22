0ea0ec825a3e26134820f5b9532e0033bcfcc40c
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-23 07:32:36 +0800

    Exo 101 and 102

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+/**
+ * interpolation_search: Interpolation Search
+ * @array: Array
+ * @size: Size
+ * @value: Value
+ * Description: Search using interpolation search
+ * Return: Search element index
+*/
+int interpolation_search(int *array, size_t size, int value)
+{
+
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,6 @@ int binary_search(int *array, size_t size, int value);
 int search_algo(int *array, size_t start, size_t end, int value);
 void print_array(int *array, size_t start, size_t end);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGO */
