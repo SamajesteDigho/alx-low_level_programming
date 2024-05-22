e44f9738ef97c9ea1988961261ff3fcacd99e78f
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-23 07:22:59 +0800

    Exo 100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,41 +11,41 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-    size_t b_size, low, top, i;
-    if (array == NULL || size == 0)
-        return (-1);
-    b_size = sqrt(size);
-    low = 0;
-    top = b_size;
-    printf("Value checked array[%ld] = [%d]\n", low, array[low]);
-    if (array[low] > value)
-        return (-1);
-    while (top + 1 <= size)
-    {
-        if (value <= array[top])
-        {
-            printf("Value found between indexes [%ld] and [%ld]\n", low, top);
-            break;
-        }
-        else
-        {
-            printf("Value checked array[%ld] = [%d]\n", top, array[top]);
-            low = top;
-            top += b_size;
-            if (top > size)
-                printf("Value found between indexes [%ld] and [%ld]\n", low, top);
-        }
-    }
-    if (top >= size)
-    {
-        printf("Value checked array[%ld] = [%d]\n", low, array[low]);
-        return (-1);
-    }
-    for (i = low; i < top + 1; i++)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-    return (-1);
+size_t b_size, low, top, i;
+if (array == NULL || size == 0)
+return (-1);
+b_size = sqrt(size);
+low = 0;
+top = b_size;
+printf("Value checked array[%ld] = [%d]\n", low, array[low]);
+if (array[low] > value)
+return (-1);
+while (top + 1 <= size)
+{
+if (value <= array[top])
+{
+printf("Value found between indexes [%ld] and [%ld]\n", low, top);
+break;
+}
+else
+{
+printf("Value checked array[%ld] = [%d]\n", top, array[top]);
+low = top;
+top += b_size;
+if (top > size)
+printf("Value found between indexes [%ld] and [%ld]\n", low, top);
+}
+}
+if (top >= size)
+{
+printf("Value checked array[%ld] = [%d]\n", low, array[low]);
+return (-1);
+}
+for (i = low; i < top + 1; i++)
+{
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
+}
+return (-1);
 }
